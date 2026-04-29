721a5e88c7e0a945161d5c9c5f95a9a58f39ebe8
Skitt-001 <190195566+Skitt-001@users.noreply.github.com>
2026-04-29 08:50:33 +0300

    changed config.js#

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/skanalv/",
+  base: "/my-vite-site/",
   build: {
     rollupOptions: {
       output: {
